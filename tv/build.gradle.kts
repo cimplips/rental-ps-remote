b9433411c16ca6acbce535412b0406a0595392f0
CimpliPS <cimplips@gmail.com>
2026-09-05 20:35:11 +0700

    Update build.gradle.kts

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -1,5 +1,30 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
-android { namespace = "com.rentalps.tv"; compileSdk = 35
-    defaultConfig { applicationId = "com.rentalps.tv"; minSdk = 23; targetSdk = 35; versionCode = 1; versionName = "1.0.0" }
+android {
+    namespace = "com.rentalps.tv"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.rentalps.tv"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
